17cfeeda20cf1c61831c6ff23025040ec0a88845
ThomasH94 <35856664+ThomasH94@users.noreply.github.com>
2023-02-04 21:09:11 -0800

    - Stubbed in Basic Functionality to the Audio Manager

--- GGJ-2023/Assets/Game/Scripts/Audio Systems/AudioManager.cs
+++ GGJ-2023/Assets/Game/Scripts/Audio Systems/AudioManager.cs
@@ -1,18 +1,59 @@
+using OliverLoescher;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AudioManager : MonoBehaviour
+public class AudioManager : Singleton<AudioManager>, ISingleton
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	[SerializeField]
+	private AudioSource sfxSource;
+	[SerializeField]
+	private AudioSource musicSource;
+	[SerializeField]
+	private AudioSource voicesSource;
+	[SerializeField]
+	private AudioClip[] audioClips;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	public void PlaySFX(AudioClip sfxClip, bool playOneShot = false)
+	{
+		if (playOneShot)
+		{
+			sfxSource.clip = sfxClip;
+			sfxSource.PlayOneShot(sfxClip);
+		}
+		else
+		{
+			sfxSource.Stop();
+			sfxSource.clip = sfxClip;
+			sfxSource.Play();
+		}
+	}
+
+	public void PlayMusic(AudioClip musicClip)
+	{
+		if (SameAudioClip(musicSource, musicClip)) return;
+
+		musicSource.Stop();
+		musicSource.clip = musicClip;
+		musicSource.Play();
+	}
+
+	private void PlayRandomMusic()
+	{
+		int randomClipIndex = UnityEngine.Random.Range(0, audioClips.Length);
+		if (SameAudioClip(musicSource, audioClips[randomClipIndex])) return;
+
+		musicSource.clip = audioClips[randomClipIndex];
+		musicSource.Play();
+	}
+
+	private bool SameAudioClip(AudioSource source, AudioClip clip)
+	{
+		return source.clip == clip;
+	}
+
+	public void OnAccessed()
+	{
+
+	}
 }
